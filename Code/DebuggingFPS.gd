515a87d1de3e6602c5c76996d25e6039551744bf
blendegames <mathcar@skiff.com>
2025-10-25 13:46:00 -0400

    Printing slower for performance boost for lower end systems
    gyaatttttt

--- Code/DebuggingFPS.gd
+++ Code/DebuggingFPS.gd
@@ -1,9 +1,15 @@
 extends Node3D
 
+const TIMER_LIMIT = 1.0
+var timer = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(str(Engine.get_frames_per_second()))
+	timer += delta
+	if timer > TIMER_LIMIT:
+		print(str(Engine.get_frames_per_second()))
+		timer = 0.0
